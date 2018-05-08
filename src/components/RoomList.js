12e3c364b605627e920052bdb60078eed8333399
Andy Prickett <andy@andyprickett.com>
2018-05-08 13:27:53 -0700

    Checkpoint 03 Create Chat Rooms: completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,10 +4,13 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
 
   }
 
@@ -19,6 +22,23 @@ class RoomList extends Component {
     });
   }
 
+  handleChange(event) {
+    let newRoomData = event.target.value;
+    this.setState(
+      { newRoomName: newRoomData }
+    );
+  }
+
+  createRoom(event) {
+    event.preventDefault();
+    this.roomsRef.push(
+      { name: this.state.newRoomName }
+    );
+    this.setState(
+      { newRoomName: '' }
+    );
+  }
+
   render() {
     return (
       <div>
@@ -27,6 +47,16 @@ class RoomList extends Component {
             (room, index) => <h1 key={index}>{room.name}</h1>
           )
         }
+        <form className="create-room" onSubmit={this.createRoom}>
+          <label>
+            <span>Create a New Room: </span>
+            <input type="text"
+                   value={this.state.newRoomName}
+                   onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Go!" />
+        </form>
       </div>
     );
   }
